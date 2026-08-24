71b3ea1c55f53e801844b28aa5888e01166ceb3b
Sean <hayesean@hotmail.com>
2026-08-24 18:40:01 -0400

    Refactor password_checker.py for clarity and structure

--- password_checker.py
+++ password_checker.py
@@ -1,18 +1,17 @@
-# Collect the account or system name.
+# Gets the name of the account or system.
 account = input("Account or system: ")
 
-# Collect the username even though it is not used for a check this week.
-# The username will be needed for future versions of the program.
+# Gets you the username.
 username = input("Username: ")
 
 # Collect the password that will be analyzed.
 password = input("Password: ")
 
-# Collect the rotation interval as a string, then convert it to an integer.
+#Gives you the rotation interval as a string, then convert it to an integer.
 rotation_interval = input("Rotation interval (months): ")
 rotation_interval = int(rotation_interval)
 
-# Calculate the required password measurements.
+# Calculates the required password measurements.
 password_length = len(password)
 length_score = password_length * 10
 rotation_count = 36 // rotation_interval
